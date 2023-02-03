956e18cb5b1dcd5e7dec49ed5bad6b04a58c095c
Selvi Ece Dugan <selvidugan@gmail.com>
2023-02-03 14:41:12 +0300

    completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import SignUp from "./SignUp"
+
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <Switch>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/messages">
+                    <Messages />
+                </Route>
+                <Route exact path="/signup">
+                    <SignUp />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
